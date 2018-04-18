7e9f4a78e13200c13daa3e67663d56dbd545c40d
Fabio Del Bene <delbio87@gmail.com>
2018-04-18 07:14:44 +0200

    fixed use of object.__str__()

--- src/State.py
+++ src/State.py
@@ -12,14 +12,14 @@ class State(StateInterface):
         return list(self._actionMap.values())
     def getAction(self, actionName):
         if actionName not in self._actionMap:
-            raise ValueError('Invalid action '+actionName+' in state '+self)
+            raise ValueError('Invalid action '+actionName+' in state '+self.__str__())
         else:
             return self._actionMap[actionName]
     def addAction(self, action):
         if action.getOriginState() != self:
             raise ValueError("L'origine dell'azione deve essere questo stato")
         if action.getName() in self._actionMap:
-            raise ValueError("cannot add action "+action.getName()+" to "+self+": state already has an action with the same name")
+            raise ValueError("cannot add action "+action.getName()+" to "+self.__str__()+": state already has an action with the same name")
         self._actionMap[action.getName()] = action
     def __str__(self):
         return self.getName()+'[class='+self.__class__.__name__+']'
