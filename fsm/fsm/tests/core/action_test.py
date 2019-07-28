aa31e579aa3c5075d1a6059225c8dab5681a67a8
Fabio Del Bene <delbio87@gmail.com>
2019-07-28 23:57:49 +0200

    fixed test import

--- fsm/fsm/tests/core/action_test.py
+++ fsm/fsm/tests/core/action_test.py
@@ -1,12 +1,12 @@
 import unittest
 
-from automaton.core.Action import Action
+from fsm.core import action as _act
 
 class TestActionMethods(unittest.TestCase):
 
     def test_constructor_nullOriginState_throwError(self):
         with self.assertRaises(ValueError):
-            action = Action(None)
+            action = _act.Action(None)
 
 if __name__ == '__main__':
     unittest.main()
