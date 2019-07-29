6e734901f23e4a6790b22546f86566bcca608161
Fabio Del Bene <delbio87@gmail.com>
2019-07-29 19:01:02 +0200

    license

--- fsm/setup.py
+++ fsm/setup.py
@@ -42,6 +42,7 @@ setup(
         # Specify the Python versions you support here. In particular, ensure
         # that you indicate whether you support Python 2, Python 3 or both.
         'Programming Language :: Python :: 3',
+         'Operating System :: OS Independent',
     ],
     keywords= 'sample automata finite state machine engine',  # Optional
 )
